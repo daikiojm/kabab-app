949ad0207feac9a08ed8b3976fc0c59c1ad9d404
daikiojm <daiki.ojima@gmail.com>
2025-03-01 19:56:18 +0900

    chore(global): empty commit for testing

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = async function (env, argv) {
     mode: 'production',
     platform: 'web',
     web: {
-      // publicPath: '/kabab-app/',
+      publicPath: '/kabab-app/',
       bundler: 'webpack',
     },
   }
