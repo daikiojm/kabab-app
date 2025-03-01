8c842d337f2e7a683e450ebb764fc98bda215fd4
daikiojm <daiki.ojima@gmail.com>
2025-03-01 20:03:04 +0900

    fix(webpack): change asset paths to relative for GitHub Pages deployment

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = async function (env, argv) {
     mode: 'production',
     platform: 'web',
     web: {
-      publicPath: './',
+      publicPath: './kabab-app/',
       bundler: 'webpack',
     },
   }
@@ -23,7 +23,7 @@ module.exports = async function (env, argv) {
   // Set base path for all assets
   config.output = {
     ...config.output,
-    publicPath: './',
+    publicPath: './kabab-app/',
   }
 
   // Configure HTML webpack plugin
